557e7a5e17958dacf09438fe74f28abb0acb9736
ponmathi <ponmathittl@gmail.com>
2023-10-08 16:55:30 +0530

    add to cart fixes

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice ( {
             state.totalQuantity++
             if (!existingItem) {
                 state.items.push({
-                    id: newItem.id,
+                    itemId: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
@@ -22,7 +22,18 @@ const cartSlice = createSlice ( {
                 existingItem.totalPrice=existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart() {}
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find (item => item.id === id);
+            state.totalQuantity--;
+            if(existingItem.quantity === 1){
+                state.items = state.items.filter(item => item.id !== id);
+            }
+            else{
+                existingItem.quantity--;
+                // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
     },
 });
 
