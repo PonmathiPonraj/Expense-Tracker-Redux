86381df28bf727e876ae2edb5a108c13f6e5117d
ponmathi <ponmathittl@gmail.com>
2023-10-08 22:25:25 +0530

    fixing the cart pblms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App () {
+    const dispatch = useDispatch();
     const showCart = useSelector((state) => state.ui.cartIsVisible);
     const cart = useSelector((state) => state.cart);
+    const notifcation = useSelector((state) => state.ui.notifcation);
 
     useEffect(() => {
-        fetch('https://react-http-2fdf7-default-rtdb.firebaseio.com/cart.json', { 
-            method: 'PUT', 
-            body: JSON.stringify(cart), 
-        }); 
-    }, [cart]);
+        dispatch(fetchCartData);
+    }, [dispatch]);
+
+    useEffect(() => {
+        if(isInitial) {
+            isInitial = false;
+            return;
+        }
+        
+        dispatch(sendCartData(cart));
+
+    }, [cart, dispatch]);
 
     return (
-        <Layout>
-            {showCart && <Cart />}
-            <Products />
-        </Layout>
-    )
+        <Fragment>
+            {notifcation && (
+                <Notification 
+                    status = {notifcation.status}
+                    title = {notifcation.title}
+                    message = {notifcation.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
+    );
 };
 
 export default App;
